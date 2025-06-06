594819d26a7ca1b050ea7209a4ece09233967632
YuriyKrasnobokiy <91632730+YuriyKrasnobokiy@users.noreply.github.com>
2025-06-06 13:53:20 +0000

    Deploying to gh-pages from @ YuriyKrasnobokiy/ts-react-vite-template@b86541aa31f44c2da5fd712f2ba99ddb6b84a642 🚀

--- assets/NotFound-3rRJ6VAq.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-D7Kp302Y.js";const n=()=>o.jsx(o.Fragment,{children:o.jsx("h2",{children:"Ooops... Page not found"})});export{n as default};
---
+++ assets/NotFound-CZR98kMt.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-CUn4DI8p.js";const n=()=>o.jsx(o.Fragment,{children:o.jsx("h2",{children:"Ooops... Page not found"})});export{n as default};
