b86541aa31f44c2da5fd712f2ba99ddb6b84a642
YKrasnobokyi <krasnobokiy.yuriy@gmail.com>
2025-06-06 16:52:53 +0300

    fix deploy and basename

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ts-react-vite-template/',
   plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: "./dist",
+    assetsDir: "assets",
+  },
+  base: '/ts-react-vite-template/',
 })
